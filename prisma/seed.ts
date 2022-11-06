8fa3dad50048360d38e68e008cf39a1c17b3b933
Louis pc <louis.chovaneck@gmail.com>
2022-11-06 11:35:34 +0100

    add sqlite db + prisma + seed

---
+++ prisma/seed.ts
@@ -0,0 +1,36 @@
+import { PrismaClient } from '@prisma/client';
+import fete from '../public/feteByDate.json';
+
+const prisma = new PrismaClient();
+
+async function main() {
+    const date = new Date().toISOString();
+
+    await prisma.fete.deleteMany({});
+
+    const inserts = [];
+    for (const [key, value] of Object.entries(fete)) {
+        for (const fete of value) {
+            inserts.push(prisma.fete.create({
+                data: {
+                    prenom: fete.pnom,
+                    createdAt: date,
+                    fete_religieuse: fete.majeur ? 1 : 0,
+                    date: key,
+                }
+            }));
+        }
+    }
+    await prisma.$transaction(inserts);
+}
+
+
+main()
+    .then(async () => {
+        await prisma.$disconnect()
+    })
+    .catch(async (e) => {
+        console.error(e)
+        await prisma.$disconnect()
+        process.exit(1)
+    })
