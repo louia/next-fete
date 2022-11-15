3b4042288bddc18cc62e11b0e780c667551e3307
Louis pc <louis.chovaneck@gmail.com>
2022-11-15 20:57:43 +0100

    api button ok

--- components/InputFete/ValidButton.tsx
+++ components/InputFete/ValidButton.tsx
@@ -1,7 +1,5 @@
 import { Button } from "@mantine/core";
-import * as ics from 'ics';
-import { EventAttributes } from "ics";
-import useDownloadCalendarFile from "../../hooks/useDownloadCalendarFile";
+import { useEffect, useState } from "react";
 import useLocalStorage from "../../hooks/useLocalStorage";
 import { Fete } from "../../pages";
 
@@ -11,48 +9,43 @@ interface Props {
 }
 
 export default function ValidButton({ selectedFetes, setSelectedFetes }: Props) {
-  const [prenomsStorage, setPrenomsStorage] = useLocalStorage('prenoms', []);  
+  const [prenomsStorage, setPrenomsStorage] = useLocalStorage('prenoms', []);
 
-  function downloadIcs() {
-    const events = [] as EventAttributes[];
-    for (const fete of selectedFetes) {
-      const dateSplit = fete?.date?.split('/');
-      if (!dateSplit) return;
-      const [day, month] = dateSplit;
-      const event = {
-        start: [Number(new Date().getFullYear()), Number(month), Number(day)],
-        end: [Number(new Date().getFullYear()), Number(month), Number(day) + 1],
-        recurrenceRule: 'FREQ=YEARLY',
-        title: `Fête de ${fete.prenom}`,
-        // url: fete.lien,
-        description: `C'est la fête des ${fete.prenom} ! Pour plus d'informations, consultez ce lien : ${''}`,
-        categories: ['fete'],
-        alarms: [{
-          action: 'display',
-          description: 'Reminder',
-          trigger: {
-            hours: 5,
-            minutes: 30,
-            before: true,
-          },
-        }],
-      } as EventAttributes;
-      events.push(event);
+  const [ids, setIds] = useState<string[] | undefined>(undefined);
+
+  useEffect(() => {
+    if (selectedFetes) {
+      const ids = selectedFetes.map(fete => fete.id);
+      const urlParam = new URLSearchParams(ids.map(s => ['ids', s])).toString();
+
+      setIds(urlParam);
     }
-    const { error, value } = ics.createEvents(events);
-    if (error) {
-      console.error(error);
-      return;
-    }
-    if (value) {
-      useDownloadCalendarFile(value, 'fete.ics');
-      setPrenomsStorage(selectedFetes.map(fete => fete.id))      
-    }
+  }, [selectedFetes])
+
+  function clickButton(e: React.MouseEvent<HTMLElement>) {
+    setPrenomsStorage(selectedFetes.map(fete => fete.id));
+    console.log('ok');
+    
+    
+  //   e.preventDefault();
+  //   e.stopPropagation();
+  // e.nativeEvent.stopImmediatePropagation();
+  }
+
+  const button = (
+    <Button color="sand.5" radius="lg" size="lg" disabled={selectedFetes.length === 0} style={{
+      width: '100%'
+    }}>
+      Valider
+    </Button>
+  );
+  if (selectedFetes.length === 0) {
+    return button;
   }
 
   return (
-    <Button color="sand.5" radius="lg" size="lg" disabled={selectedFetes.length === 0} onClick={downloadIcs}>
-      Valider
-    </Button>
+    <a href={`/api/fete/fete.ical?${ids}`} onClick={clickButton}>
+      {button}
+    </a>
   );
 }
