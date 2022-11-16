9302ecde80abe5515c2dacaf4d4f8ce2c26bed86
Louis pc <louis.chovaneck@gmail.com>
2022-11-16 19:58:11 +0100

    refacto logic for web view

--- pages/api/fete/fete.ical.ts
+++ pages/api/fete/fete.ical.ts
@@ -1,9 +1,8 @@
 import { PrismaClient } from '@prisma/client';
-import * as ics from 'ics';
-import { EventAttributes } from 'ics';
 import type { NextApiRequest, NextApiResponse } from 'next';
 import stream from 'stream';
 import { promisify } from 'util';
+import createCalendarEvents from '../../../utils/createCalendarEvents';
 
 const prisma = new PrismaClient();
 
@@ -25,7 +24,6 @@ export default async function handler(
   }
 
   if (!selectedFetesIds) return res.status(405).send({ message: 'id vide' });
-  console.log(selectedFetesIds);
 
   const selectedFetes = await prisma.fete.findMany({
     where: {
@@ -43,39 +41,11 @@ export default async function handler(
     }
   });
 
-  const events = [] as EventAttributes[];
-  for (const fete of selectedFetes) {
-    const dateSplit = fete?.date?.split('/');
-    if (!dateSplit) return;
-    const [day, month] = dateSplit;
-    const event = {
-      start: [Number(new Date().getFullYear()), Number(month), Number(day)],
-      end: [Number(new Date().getFullYear()), Number(month), Number(day) + 1],
-      recurrenceRule: 'FREQ=YEARLY',
-      title: `Fête de ${fete.prenom}`,
-      // url: fete.lien,
-      description: `C'est la fête des ${fete.prenom} ! Pour plus d'informations, consultez ce lien : ${''}`,
-      categories: ['fete'],
-      alarms: [{
-        action: 'display',
-        description: 'Reminder',
-        trigger: {
-          hours: 5,
-          minutes: 30,
-          before: true,
-        },
-      }],
-    } as EventAttributes;
-    events.push(event);
-  }
-  const { error, value } = ics.createEvents(events);
-  if (error) {
-    console.error(error);
-    return;
-  }
-  if (value) {
+  const ics = createCalendarEvents(selectedFetes);
+
+  if (ics) {
     res.setHeader('Content-Type', 'text/calendar');
     res.setHeader('Content-Disposition', 'attachment; filename=fete.ics');
-    await pipeline(value, res);
+    await pipeline(ics, res);
   }
 }
---
+++ utils/createCalendarEvents.ts
@@ -0,0 +1,42 @@
+import * as ics from 'ics';
+import { EventAttributes } from "ics";
+import { Fete } from '../pages';
+
+export default function createCalendarEvents(selectedFetes: Fete[]|{
+    prenom: string|null,
+    date: string|null,
+    genre: string|null,
+}[]): string|undefined {
+    const events = [] as EventAttributes[];
+    for (const fete of selectedFetes) {
+      const dateSplit = fete?.date?.split('/');
+      if (!dateSplit) return;
+      const [day, month] = dateSplit;
+      const event = {
+        start: [Number(new Date().getFullYear()), Number(month), Number(day)],
+        end: [Number(new Date().getFullYear()), Number(month), Number(day) + 1],
+        recurrenceRule: 'FREQ=YEARLY',
+        title: `Fête de ${fete.prenom}`,
+        // url: fete.lien,
+        description: `C'est la fête des ${fete.prenom} (${fete.genre}) ! Pour plus d'informations, consultez ce lien : ${''}`,
+        categories: ['fete'],
+        alarms: [{
+          action: 'display',
+          description: 'Reminder',
+          trigger: {
+            hours: 5,
+            minutes: 30,
+            before: true,
+          },
+        }],
+      } as EventAttributes;
+      events.push(event);
+    }
+    const { error, value } = ics.createEvents(events);
+    if (error) {
+      console.error(error);
+      return;
+    }
+
+    return value;
+}
